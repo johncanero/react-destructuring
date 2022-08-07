6ce73e6ff32d86aeba292dc0b63f1dc94f635c32
John Canero <johncanero.official@gmail.com>
2022-08-08 07:10:45 +0800

    Include Contents from Fork Code Sandbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import cars from './cars';
 
-function App() {
+  function App() {
+
+  // Console Log
+  const [honda, tesla] = cars;
+  console.log(honda);
+
+  // SpeedStats
+  const {
+    speedStats: { topSpeed: hondaTopSpeed }
+  } = honda;
+
+  const {
+    speedStats: { topSpeed: teslaTopSpeed }
+  } = tesla;
+
+  // coloursByPopularity
+  const {
+    coloursByPopularity: [hondaTopColour]
+  } = honda;
+
+  const {
+    coloursByPopularity: [teslaTopColour]
+  } = tesla;
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <table>
+            <tbody>
+              <tr>
+                <th>Brand</th>
+                <th>Top Speed</th>
+              </tr>
+              <tr>
+                <td>{tesla.model}</td>
+                <td>{teslaTopSpeed}</td>
+                <td>{teslaTopColour}</td>
+              </tr>
+              <tr>
+                <td>{honda.model}</td>
+                <td>{hondaTopSpeed}</td>
+                <td>{hondaTopColour}</td>
+              </tr>
+            </tbody>
+     </table>
     </div>
   );
 }
